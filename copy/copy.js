c84de460e8df6fdd1b351ae0df3e8e82afa68665
Krzysiek-89 <80069899+Krzysiek-89@users.noreply.github.com>
2021-06-27 11:20:26 +0200

    addis locations list to main page

--- copy/copy.js
+++ copy/copy.js
@@ -17,6 +17,10 @@ const main = async () => {
     const data = await fetch(
       "https://rickandmortyapi.com/api/character?page=1"
     );
+    function locations() {
+      const locations = docuents.getElementById("locations");
+      locations.innerHTML = "xxx";
+    }
     const json = await data.json();
     const $lista = document.getElementById("lista");
     $strona.innerHTML = 1;
